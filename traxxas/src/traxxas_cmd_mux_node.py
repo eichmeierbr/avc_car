e6ac70a3317cbb99d29acfa3de046ed364b327ec
Sebastian Helzer <derek.workman@aggiemail.usu.edu>
2019-04-26 13:07:26 -0600

    First Hardware Trial

--- traxxas/src/traxxas_cmd_mux_node.py
+++ traxxas/src/traxxas_cmd_mux_node.py
@@ -3,12 +3,17 @@
 import rospy
 from std_msgs.msg import Float64
 from sensor_msgs.msg import Joy
+from dynamic_reconfigure.server import Server
+from traxxas.cfg import motorConfig
 
 #freq_p1 = rospy.Rate(0.5)	#required frequency
 T_p1 = 2	#2 seconds
 time_p1 = 0.0
 #last_time_p1 = 0.0
 #flag_p1 = False
+
+min_speed = 1.245
+max_speed =  5
 
 def set_time_p1(time):
     global time_p1
@@ -22,20 +27,14 @@ def p1_servo_callback(msg):
 def p1_vel_callback(msg):
     global vel_pub
     set_time_p1(rospy.Time.now().to_sec())
+    if(msg.data > 0):
+        msg.data=min_speed+msg.data*(max_speed-min_speed)
+    elif(msg.data < 0):
+        msg.data=-min_speed+msg.data*(max_speed-min_speed)
+    else:
+        msg.data=0
+        
     vel_pub.publish(msg)
-
-
-def p2_servo_callback(msg):
-    #global servo_pub
-    if (rospy.Time.now().to_sec() - time_p1) > T_p1:
-        # msg.data*=7.5
-        servo_pub.publish(msg)
-
-def p2_vel_callback(msg):
-    #global vel_pub
-    if (rospy.Time.now().to_sec() - time_p1) > T_p1:
-        msg.data*=2
-        vel_pub.publish(msg)
 
 def joyCallback(msg):
     global motor_topic, servo_topic, current_topic_motor, current_topic_servo
@@ -66,6 +65,14 @@ def joyCallback(msg):
         servo_topic = rospy.Subscriber(current_topic_servo, Float64, p1_vel_callback)
         
 
+def reconfig_callback(config,level):
+    global min_speed
+    global max_speed
+    min_speed=config['Min_Speed']
+    max_speed=config['Max_Speed']
+    return config
+
+
 if __name__=='__main__':
     try:
         rospy.init_node('traxxas_cmd_mux')
@@ -73,9 +80,14 @@ if __name__=='__main__':
         # current_topic_servo = '/cmd/set_motor_vel'
         current_topic_motor = '/joy/set_servo_pos'
         current_topic_servo = '/joy/set_motor_vel'
-        motor_topic = rospy.Subscriber(current_topic_motor, Float64, p2_servo_callback)
-        servo_topic = rospy.Subscriber(current_topic_servo, Float64, p2_vel_callback)
+
+        Server(motorConfig,reconfig_callback)
+
+        motor_topic = rospy.Subscriber(current_topic_motor, Float64, p1_servo_callback)
+        servo_topic = rospy.Subscriber(current_topic_servo, Float64, p1_vel_callback)
+        
         rospy.Subscriber('/joy', Joy, joyCallback)
+
         servo_pub = rospy.Publisher('/set_servo_pos', Float64, queue_size=1)
         vel_pub = rospy.Publisher('/set_motor_vel', Float64, queue_size=1)
 
